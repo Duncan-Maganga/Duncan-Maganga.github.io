507a9747db32ee51c94abf3c8c9f16cff8bb7196
Zerotrust-debug <duncanmaganga100@gmail.com>
2025-09-07 09:47:03 -0400

    Add changes

--- index.js
+++ index.js
@@ -56,8 +56,8 @@ startMatrixAnimation();
 const projects = [
     { title: "Project 1", description: "Network vulnerability scanner with real-time reporting.", link: "https://github.com/Duncan-Maganga/project1", markdown: "projects/project1.md" },
    // { title: "Project 2", description: "Cloud security assessment tool for AWS and Azure environments.", link: "https://github.com/Duncan-Maganga/project2", markdown: "project2.md" },
-   // { title: "Project 3", description: "Incident response automation platform for SOC teams.", link: "https://github.com/Duncan-Maganga/project3", markdown: "project3.md" },
-   // { title: "Project 4", description: "Developed a phishing simulation campaign using Gophish.", link: "https://github.com/Duncan-Magangaa", markdown: "project4.md" },
+   // { title: "Project 3", description: "Designed and executed a phishing campaign to demonstrate how attackers trick users and how organizations can strengthen defenses through awareness training and simulations.", link: "https://github.com/Duncan-Maganga/project3", markdown: "project3.md" },
+    { title: "Project 4", description: "Designed and executed a phishing campaign to demonstrate how attackers trick users and how organizations can strengthen defenses through awareness training and simulations.", link: "https://github.com/Duncan-Maganga/Phishing-Simulation-with-GoPhish", markdown: "projects/project1.md" },
     { title: "Project 5", description: "Built an automated vulnerability scanner with Python.", link: "https://github.com/Duncan-MagangaA", markdown: "project5.md" },
     { title: "Project 6", description: "Configured SIEM rules for real-time incident detection.", link: "https://github.com/Duncan-Magangab", markdown: "project6.md" },
     { title: "Project 7", description: "Created a malware sandbox for safe binary execution.", link: "https://github.com/Duncan-MagangAB", markdown: "project7.md" },
